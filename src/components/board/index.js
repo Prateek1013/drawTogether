5100920bf9278f42238c825f850b25ab1f17da27
Prateek1013 <prateek9771196112@gmail.com>
2023-12-29 21:47:54 +0530

    canvas working stroke+color

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -1,14 +1,50 @@
 import { useEffect, useRef } from "react";
-
+import { useSelector } from "react-redux";
 const Board = () => {
   const canvas_ref = useRef(null);
-  useEffect(() => {
-    if (!canvas_ref.current) return;
-    const canvas=canvas_ref.current;
-    const context=canvas.getContext('2d');
+  const draw = useRef(false);
+  const activemenu = useSelector((state) => state.menu.activemenu);
+  const { color, size } = useSelector((state) => state.toolbox[activemenu]);
 
-    canvas.width=window.innerWidth;
-    canvas.height=window.innerHeight;
+  useEffect(() => {
+    if (!canvas_ref) return;
+    const canvas = canvas_ref.current;
+    const context = canvas.getContext("2d");
+
+    context.strokeStyle = color;
+    context.lineWidth = size;
+  }, [color, size]);
+
+  useEffect(() => {
+    // this is for mounting
+    if (!canvas_ref.current) return;
+    const canvas = canvas_ref.current;
+    const context = canvas.getContext("2d");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const handlemousedown = (e) => {
+      draw.current = true;
+      context.beginPath();
+      context.moveTo(e.clientX, e.clientY);
+    };
+    const handlemousemove = (e) => {
+      if (!draw.current) return;
+      context.lineTo(e.clientX, e.clientY);
+      context.stroke();
+    };
+    const handlemouseup = () => {
+      draw.current=false;
+    };
+
+    canvas.addEventListener("mousedown", handlemousedown);
+    canvas.addEventListener("mousemove", handlemousemove);
+    canvas.addEventListener("mouseup", handlemouseup);
+    return () => {
+      canvas.removeEventListener("mousedown", handlemousedown);
+      canvas.removeEventListener("mousemove", handlemousemove);
+      canvas.removeEventListener("mouseup", handlemouseup);
+    };
   }, []);
   return <canvas ref={canvas_ref}></canvas>;
 };
--- src/components/toolbox/index.js
+++ src/components/toolbox/index.js
@@ -1,37 +1,54 @@
 import styles from "./index.module.css";
 import { COLORS, MENU_ITEMS } from "@/constants";
-import { useSelector,useDispatch } from "react-redux";
-import { changeColor,chnageBrushSize } from "@/slices-redux/toolboxSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { changeColor, chnageBrushSize } from "@/slices-redux/toolboxSlice";
 const ToolBox = () => {
-  const activemenu=useSelector(state=>state.menu.activemenu);
-  const size_val=useSelector(state=>state.toolbox[activemenu].size);
+  const activemenu = useSelector((state) => state.menu.activemenu);
+  const size_val = useSelector((state) => state.toolbox[activemenu].size);
 
-  const showcolors=activemenu!==MENU_ITEMS.ERASER;
-  const dispatch=useDispatch();
+  const showcolors = activemenu !== MENU_ITEMS.ERASER;
+  const dispatch = useDispatch();
   return (
     <div className={styles.toolboxContainer}>
-      {showcolors && <div className={styles.toolItem}>
-        <h4 className={styles.toolText}>Stroke Color</h4>
-        <div className={styles.itemContainer}>
-          {
-            COLORS.map(color=>(
-                <div className={styles.colorBox}  style={{backgroundColor:color}} onClick={(e)=>{
-                  dispatch(changeColor({
-                    item:activemenu,
-                    color
-                  }))
-                }}/>
-            ))
-          }
+      {showcolors && (
+        <div className={styles.toolItem}>
+          <h4 className={styles.toolText}>Stroke Color</h4>
+          <div className={styles.itemContainer}>
+            {COLORS.map((color) => (
+              <div
+                className={styles.colorBox}
+                style={{ backgroundColor: color }}
+                onClick={(e) => {
+                  dispatch(
+                    changeColor({
+                      item: activemenu,
+                      color,
+                    })
+                  );
+                }}
+              />
+            ))}
+          </div>
         </div>
-      </div>}
+      )}
       <div className={styles.toolItem}>
         <h4 className={styles.toolText}>Brush Size</h4>
         <div className={styles.itemContainer}>
-          <input type="range" min={1} max={10} step={1} value={size_val} onChange={(e)=>{ dispatch(chnageBrushSize({
-            item:activemenu,
-            size:e.target.value
-          })) }}/>
+          <input
+            type="range"
+            min={1}
+            max={10}
+            step={1}
+            value={size_val}
+            onChange={(e) => {
+              dispatch(
+                chnageBrushSize({
+                  item: activemenu,
+                  size: e.target.value,
+                })
+              );
+            }}
+          />
           {size_val}
         </div>
       </div>
