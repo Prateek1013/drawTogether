89b1bd2aa3ae2437f19374d949d94052f2611a1d
Prateek1013 <prateek9771196112@gmail.com>
2024-01-05 02:17:27 +0530

    removed if from uselayouteffect

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -32,7 +32,7 @@ const Board = () => {
       context.putImageData(imgData, 0, 0);
     }
     dispatch(actionItemClick(null));
-  }, [actionmenu]);
+  }, [actionmenu,dispatch]);
 
   useEffect(() => {
     if (!canvas_ref) return;
@@ -46,69 +46,68 @@ const Board = () => {
       context.lineWidth = arg.size;
     });
   }, [color, size]);
-  if (typeof window !== "undefined") {
-    useLayoutEffect(() => {
-      // this is for mounting
-      if (!canvas_ref.current) return;
-      const canvas = canvas_ref.current;
-      const context = canvas.getContext("2d");
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
 
-      const handlemousedown = (e) => {
-        draw.current = true;
-        context.beginPath();
-        context.moveTo(e.clientX, e.clientY);
-        socket.emit("beginPath", { x: e.clientX, y: e.clientY });
-      };
-      const handlemousemove = (e) => {
-        if (!draw.current) return;
-        context.lineTo(e.clientX, e.clientY);
-        context.stroke();
-        socket.emit("drawPath", { x: e.clientX, y: e.clientY });
-      };
-      const handlemouseup = () => {
-        draw.current = false;
-        const currBoardImg = context.getImageData(
-          0,
-          0,
-          canvas.width,
-          canvas.height
-        );
-        canvasHistory.current.push(currBoardImg);
-        historyPtr.current = canvasHistory.current.length - 1;
-      };
+  useLayoutEffect(() => {
+    // this is for mounting
+    if (!canvas_ref.current) return;
+    const canvas = canvas_ref.current;
+    const context = canvas.getContext("2d");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-      canvas.addEventListener("mousedown", handlemousedown);
-      canvas.addEventListener("mousemove", handlemousemove);
-      canvas.addEventListener("mouseup", handlemouseup);
-      socket.on("anything_path", (arg) => {
+    const handlemousedown = (e) => {
+      draw.current = true;
+      context.beginPath();
+      context.moveTo(e.clientX, e.clientY);
+      socket.emit("beginPath", { x: e.clientX, y: e.clientY });
+    };
+    const handlemousemove = (e) => {
+      if (!draw.current) return;
+      context.lineTo(e.clientX, e.clientY);
+      context.stroke();
+      socket.emit("drawPath", { x: e.clientX, y: e.clientY });
+    };
+    const handlemouseup = () => {
+      draw.current = false;
+      const currBoardImg = context.getImageData(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+      );
+      canvasHistory.current.push(currBoardImg);
+      historyPtr.current = canvasHistory.current.length - 1;
+    };
+
+    canvas.addEventListener("mousedown", handlemousedown);
+    canvas.addEventListener("mousemove", handlemousemove);
+    canvas.addEventListener("mouseup", handlemouseup);
+    socket.on("anything_path", (arg) => {
+      context.beginPath();
+      context.moveTo(arg.x, arg.y);
+    });
+
+    socket.on("anything_draw", (arg) => {
+      context.lineTo(arg.x, arg.y);
+      context.stroke();
+    });
+
+    return () => {
+      canvas.removeEventListener("mousedown", handlemousedown);
+      canvas.removeEventListener("mousemove", handlemousemove);
+      canvas.removeEventListener("mouseup", handlemouseup);
+
+      socket.off("anything_path", (arg) => {
         context.beginPath();
         context.moveTo(arg.x, arg.y);
       });
 
-      socket.on("anything_draw", (arg) => {
+      socket.off("anything_draw", (arg) => {
         context.lineTo(arg.x, arg.y);
         context.stroke();
       });
-
-      return () => {
-        canvas.removeEventListener("mousedown", handlemousedown);
-        canvas.removeEventListener("mousemove", handlemousemove);
-        canvas.removeEventListener("mouseup", handlemouseup);
-
-        socket.off("anything_path", (arg) => {
-          context.beginPath();
-          context.moveTo(arg.x, arg.y);
-        });
-
-        socket.off("anything_draw", (arg) => {
-          context.lineTo(arg.x, arg.y);
-          context.stroke();
-        });
-      };
-    }, []);
-  }
+    };
+  }, []);
   return <canvas ref={canvas_ref}></canvas>;
 };
 
